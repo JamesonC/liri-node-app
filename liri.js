5606ae9153bcd738e856afbc9c94f602abbb08bc
Jameson Campbell <jcampbell@flexterapp.com>
2019-05-07 15:20:40 -0500

    OMDB API

--- liri.js
+++ liri.js
@@ -1,3 +1,42 @@
 require("dotenv").config();
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
+// var spotify = new Spotify(keys.spotify);
+var axios = require("axios");
+var operand = process.argv[2];
+var nodeArgs = process.argv;
+var movieName = "";
+
+switch (operand) {
+    case 'movie-this':
+        for (var i = 3; i < nodeArgs.length; i++) {
+
+            if (i > 2 && i < nodeArgs.length) {
+                movieName = movieName + "+" + nodeArgs[i];
+            } else {
+                movieName += nodeArgs[i];
+
+            }
+        }
+
+        // Then run a request with axios to the OMDB API with the movie specified
+        var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+        // This line is just to help us debug against the actual URL.
+        console.log(queryUrl);
+
+        axios.get(queryUrl).then(
+            function (response) {
+                console.log("Title: " + response.data.Title);
+                console.log("Release Year: " + response.data.Year);
+                console.log("IMBD Rating: " + response.data.imdbRating);
+                console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+                console.log("Country Produced: " + response.data.Country);
+                console.log("Language: " + response.data.Language);
+                console.log("Plot: " + response.data.Plot);
+                console.log("Actors: " + response.data.Actors);
+            }
+        );
+        break;
+    default:
+        console.log('Sorry!');
+}
